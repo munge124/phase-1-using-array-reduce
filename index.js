93dcda667e9727b52a863ad5621362cf1d794953
benson <bensonmunge545@gmail.com>
2025-03-12 11:29:44 +0300

    added new changes

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+
+
+const totalBatteries = batteryBatches.reduce((accum, batch) => accum + batch, 0);
+
+console.log(totalBatteries); // Outputs the total number of batteries
+
+
 // Code your solution here
